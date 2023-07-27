e6efe6182c53bee8d05cf5f387dd8710b6b40fbe
Baqytbayev <bakitbaev98@gmail.com>
2023-07-28 03:00:20 +0600

    Сделал сортировка по алфавиту а также чтоб тянулись дпугие 10 рандомов сейчас сделаю чтоб была фильтрация по гендерной принадлежности

--- src/components/getRandom/getRandom.jsx
+++ src/components/getRandom/getRandom.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import './getRandom.css'
 import {useDispatch, useSelector} from "react-redux";
 import {getRandom} from "../../store/randomSlice";
@@ -9,21 +9,49 @@ const GetRandom = () => {
     const randoms = useSelector(
         state => state.random.random.results
     )
-    console.log(randoms)
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(getRandom())
     }, [dispatch])
+    const [sort, setSort] = useState(true);
+
+    const sortByname = useSelector(state =>
+        [...state.random.random.results].sort((a, b) => a.name.first.localeCompare(b.name.first))
+    );
+
+
+    const sortBynameRevers = useSelector(state =>
+        [...state.random.random.results].sort((a, b) => b.name.first.localeCompare(a.name.first))
+    );
+
+    const sortRandoms = sort ? sortByname : sortBynameRevers;
+    const refresh = () => {
+        dispatch(getRandom());
+    };
     return (
         <div className={'randoms'}>
-            {randoms.map((random, i) =>(
-                <OneRandom
-                key={i}
-                image={random.picture.large}
-                text={random.name.first}
-                gender={random.gender}/>
+            <div className={'buttonDiv'}>
+                <button className={'button'} onClick={() => setSort(true)}>
+                    Sort by Name (A to Z)
+                </button>
+                <button className={'button'} onClick={() => setSort(false)}>
+                    Sort by Name (Z to A)
+                </button>
+                <button className={'button'} onClick={refresh}>
+                    Refresh
+                </button>
+            </div>
+            <div className={'randomInfo'}>
+                {sortRandoms.map((random, i) =>(
+                    <OneRandom
+                        key={i}
+                        image={random.picture.large}
+                        text={random.name.first}
+                        gender={random.gender}/>
                 ))
-            }
+                }
+            </div>
+
         </div>
     )
 }
