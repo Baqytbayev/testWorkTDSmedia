4c650189e54b0064c6df11bb3df92a42b03d95f3
Baqytbayev <bakitbaev98@gmail.com>
2023-07-28 01:45:46 +0600

    Сделал метод для подтягивания данных

---
+++ src/api/randomApi.js
@@ -0,0 +1,14 @@
+import {instances} from "./instances";
+
+
+class RandomApi {
+    getRandom = async () => {
+        try {
+            const response = await instances.get('?results=10')
+            return response.data
+        }catch (e) {
+            console.log(e)
+        }
+    }
+}
+export const randomApi = new RandomApi()
